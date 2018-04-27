b5eb0d6fa52fb00395354d53dbc83eb57f228979
Gerardo García Lopez <gerardoagl96@gmail.com>
2018-04-27 11:54:47 -0400

    Agregando Query-Filters package

--- resources/views/layouts/simple.blade.php
+++ resources/views/layouts/simple.blade.php
@@ -7,7 +7,7 @@
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/app.css') }}">
 	<link rel="stylesheet" type="text/css" href="{{ URL::asset('css/custom.css') }}">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<link rel="shortcut icon" href="{{{ asset('img/logo.png') }}}">
+	{{-- <link rel="shortcut icon" href="{{{ asset('img/logo.png') }}}"> --}}
 	<script src="{{ URL::asset('js/plugins/jquery/jquery-3.3.1.min.js') }}"></script>
 	<script src="{{ URL::asset('js/plugins/datatables/jquery.dataTables.js') }}"></script>
 	<script src="{{ URL::asset('js/plugins/datatables-bulma/js/dataTables.bulma.min.js') }}"></script>
